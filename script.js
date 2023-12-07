9d6dc40f30be5ba5d3ef3e4c2b2f41b0b6cdd2b2
pop-eye <alasdairswenson@gmail.com>
2023-12-07 13:35:55 +0000

    added unmute when displayed

--- script.js
+++ script.js
@@ -6,18 +6,25 @@ document.addEventListener("DOMContentLoaded", function() {
             if (entry.isIntersecting) {
                 entry.target.play().then(() => {
                     // Play started successfully
+                    entry.target.muted = false; // Unmute the video when in view
                 }).catch(err => {
                     console.error('Error attempting to play video:', err);
                 });
             } else {
                 entry.target.pause();
+                entry.target.muted = true; // Mute the video when out of view
             }
         });
     }, { threshold: 0.5 });
 
     videos.forEach(video => {
-        video.setAttribute('muted', ''); // Muting the video
+        video.setAttribute('muted', ''); // Initially mute the video
         video.setAttribute('loop', '');  // Ensuring loop
         observer.observe(video);
+
+        // Optional: Add click listener to toggle mute
+        video.addEventListener('click', function() {
+            this.muted = !this.muted;
+        });
     });
 });
